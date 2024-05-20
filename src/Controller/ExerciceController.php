91ff4d6ef01e010d653404c89030d2d51dff7f39
mohamedtatahdev <mohamed.tatah69@gmail.com>
2024-05-20 20:48:27 +0200

    third commit

--- src/Controller/ExerciceController.php
+++ src/Controller/ExerciceController.php
@@ -7,6 +7,7 @@ use App\Entity\Comment;
 use App\Entity\Exercice;
 use App\Form\CommentType;
 use App\Form\ExerciceType;
+use App\Repository\CommentRepository;
 use App\Repository\VoteRepository;
 use App\Repository\ExerciceRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -26,8 +27,8 @@ class ExerciceController extends AbstractController
         ]);
     }
 
-    #[Route('/{slug}', name: 'exercice_show')]
-    public function category($slug, ExerciceRepository $exerciceRepository,Request $request, EntityManagerInterface $em): Response
+    #[Route('/{slug}/{id?}', name: 'exercice_show')]
+    public function category($slug, ?int $id = null, ExerciceRepository $exerciceRepository,Request $request, EntityManagerInterface $em,CommentRepository $commentRepository): Response
     {
         $exercice = $exerciceRepository->findOneBySlug($slug);//recupere les cat en fonction du slug
 
@@ -36,27 +37,58 @@ class ExerciceController extends AbstractController
         }
 
         $user = $this->getUser();
+        $comment = $id ? $commentRepository->find($id) : new Comment();
 
-        $comment = new Comment();
         $commentForm = $this->createForm(CommentType::class, $comment);
         $commentForm->handleRequest($request);
         if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-            $comment->setCreatedAt(new \DateTimeImmutable());
-            $comment->setExercice($exercice);
-            $comment->setUser($user);
-            $em->persist($comment);
-            $em->flush();
-            return $this->redirect($request->getUri());
+          if (!$id) {
+              $comment->setCreatedAt(new \DateTimeImmutable());
+              $comment->setExercice($exercice);
+              $comment->setUser($user);
+              $em->persist($comment);
+          }
+          $em->flush();
+  
+          return $this->redirectToRoute('exercice_show', ['slug' => $slug]);
+
         }
+        
         
     
 
         return $this->render('exercice/show.html.twig', [
             'exercice' => $exercice,
             'form' => $commentForm->createView(),
+            'is_edit' => $id !== null,
+
 
         ]);
     }
+
+    #[Route('/comment/delete/{id}', name: 'comment_delete')]
+public function deleteComment(int $id, CommentRepository $commentRepository, Request $request, EntityManagerInterface $em): Response
+{
+    $comment = $commentRepository->find($id);
+
+    if (!$comment) {
+        throw $this->createNotFoundException('Le commentaire n\'existe pas');
+    }
+
+    $user = $this->getUser();
+
+    // Vérifiez si l'utilisateur connecté est bien l'auteur du commentaire
+    if ($comment->getUser() !== $user) {
+        throw $this->createAccessDeniedException('Vous n\'avez pas le droit de supprimer ce commentaire');
+    }
+
+    $exerciceSlug = $comment->getExercice()->getSlug();
+
+    $em->remove($comment);
+    $em->flush();
+
+    return $this->redirectToRoute('exercice_show', ['slug' => $exerciceSlug]);
+}
 
       #[Route('/exercice/rating/{id}/{score}', name: 'exercice_rating')]
       public function ratingQuestion(Request $request, Exercice $exercice, int $score, EntityManagerInterface $em, VoteRepository $voteRepo)
